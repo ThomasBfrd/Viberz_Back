c2c486c277c92cbb14dec9d26c243ddfd98035b6
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-03 18:47:46 +0100

    feat: ajout des interfaces pour la couche domain

---
+++ Viberz.Domain/Interfaces/Playlist/IPlaylistRepository.cs
@@ -0,0 +1,12 @@
+﻿using Viberz.Domain.Entities;
+using Viberz.Domain.Enums;
+using Viberz.Domain.Interfaces;
+
+public interface IPlaylistRepository : IBaseRepository<Playlist, int>
+{
+    Task<List<Playlist>> GetPlaylists(int page = 1, int pageSize = 5);
+    Task<List<Playlist>> GetPlaylistsByFamily(string userId, FamilyGenres family, string search, bool likes, int page = 1, int pageSize = 5);
+    Task<Playlist> UpdatePlaylist(Playlist playlist);
+    Task<bool> LikePlaylist(int playlistId, string userId);
+    Task<bool> DislikePlaylist(int playlistId, string userId);
+}
